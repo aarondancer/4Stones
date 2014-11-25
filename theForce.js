400f1c2d111b3acece09200ee606a58f2648c274
Aaron Dancer <me@aarondancer.com>
2014-11-25 17:51:21 -0600

    theForce is strong with this dialog
    I created a custom dialog class that will allow us to show messages/dialogs to the user and prompt between Cancel, No, and Yes.
    
    Added functions to theForce.js to call different kinds of dialogs

--- theForce.js
+++ theForce.js
@@ -18,6 +18,7 @@ function selectDifficulty(difficulty){
     computer.difficulty = difficulty; //Set the difficulty level of the AI
     difficultyMenu.visible = false; //Hide the difficulty selection menu
     if (difficulty === 4) aiOn = false; //If the selection was to play against friend, then turn the AI off
+    goFirst.visible = true;
 }
 
 function showDifficulties(){
@@ -29,5 +30,36 @@ function whoGoesFirst(player){ //Sets who goes first
     turn = player;
     turnColor = (turn === -1) ? red : blue;
     goFirst.visible = false;
-    if (player === -1 && aiOn === true) aiMove();
+    if (player === -1 && aiOn === true && turn === -1) aiMove();
+}
+
+function initWhoGoesFirst(){
+    firstButton.text = (player.username !== "") ? player.username : "Player 1";
+    switch(computer.difficulty){
+    case 1:
+        secondButton.text = "Easy AI";
+        break;
+    case 2:
+        secondButton.text = "Medium AI";
+        break;
+    case 3:
+        secondButton.text = "Hard AI";
+        break;
+    case 4:
+        secondButton.text = "Player 2";
+    }
+}
+
+function showWinDialog(p){
+    winDialog.message = "Player " + p + " wins!";
+    winDialog.visible = true;
+}
+
+function showDialog(title, message, cancel, decline, accept){
+    dialog.title = title;
+    dialog.message = message;
+    dialog.cancel = cancel;
+    dialog.decline = decline;
+    dialog.accept = accept;
+    dialog.visible = true;
 }
